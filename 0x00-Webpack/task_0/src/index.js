5e70dd5eac3b175b78029cbb1fcc94d3d9777797
mickiy123 <mikikinfe345@gmail.com>
2022-07-11 14:57:39 +0300

    Completed task 0

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,5 @@
+import $ from "jquery";
+
+$('body').html('<p>Dashboard data for the students</p>');
+$('p').prepend('<p>Holberton Dashboard</p>');
+$('body:nth-child(2n)').append('<p>Copyright - Holberton School</p>');

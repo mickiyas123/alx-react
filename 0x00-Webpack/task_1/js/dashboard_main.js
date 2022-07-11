92e1e141a6938343145a1f0e86cd89d3d6820ec1
mickiy123 <mikikinfe345@gmail.com>
2022-07-11 19:40:20 +0300

    Restore

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,19 +1,20 @@
-import $ from 'jquery';
-import _ from 'lodash';
-$(function() {
-let count = 0;
+import $ from "jquery"
+import _ from "lodash";
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
+$('body').append("<p id='count'></p>")
 $('body').append('<p>Copyright - Holberton School</p>');
 
-// $('button').on('click', updateCounter);
-$('button').on('click', _.debounce(updateCounter, 500));
 
+let count = 0;
 function updateCounter() {
-  count = count + 1;
-  $('#count').text(count + ' clicks on the button');
+    count += 1;
+    $('p#count').text(`${count} clicks on the button`);
 }
-});
+
+
+$('button').click(_.debounce(updateCounter, 500));
+
+z
--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,14 +1,10 @@
 const path = require('path');
 
-
 module.exports = {
-  mode: 'production',
-  entry: {
-    index: path.resolve(__dirname, 'js/dashboard_main.js'),
-  },
-
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-  }
+    mode: 'production',
+    entry: path.resolve(__dirname, 'js/dashboard_main.js'),
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',
+    },
 }

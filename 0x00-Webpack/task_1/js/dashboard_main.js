4e0012a0795b10dd3af578694cc312e9e51e10c3
mickiy123 <mikikinfe345@gmail.com>
2022-07-11 16:56:04 +0300

    Update

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -5,12 +5,13 @@ $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
 $('body').append('<p></p>').attr('id', 'count');
+$('body').append('<p>Copyright - Holberton School</p>');
 
 function updateCounter() {
     let count = 0;
-    $('button').on('click', _.debounce(()=> {
+    $('button').on('click', ()=> {
         count += 1;
         $('p#count').text(`${count} clicks on the button`);
-    }, 100));
+    });
 }
 

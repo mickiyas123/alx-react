2beb815cb5d4e2195a0090df40c834c5b5ef8c2b
mickiy123 <mikikinfe345@gmail.com>
2022-07-11 16:58:35 +0300

    Update

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,17 +1,17 @@
 import $ from "jquery";
-import _ from "lodash";
+const _ = require('lodash');
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
 $('body').append('<p></p>').attr('id', 'count');
-$('body').append('<p>Copyright - Holberton School</p>');
+$('body').append('<p>Copyright - Holberton School</p>')
 
+let count = 0;
 function updateCounter() {
-    let count = 0;
-    $('button').on('click', ()=> {
+    $('button').on('click', _.debounce(()=> {
         count += 1;
         $('p#count').text(`${count} clicks on the button`);
-    });
+    }, 100));
 }
 

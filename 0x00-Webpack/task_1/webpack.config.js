1bed57d05024b05e48a0f0a325f83c4ded8c5d58
mickiy123 <mikikinfe345@gmail.com>
2022-07-11 20:05:08 +0300

    Restore

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -2,11 +2,9 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
-    entry: {
-        bundle: path.resolve(__dirname, 'js/dashboard_main.js')
-    },
+    entry: path.resolve(__dirname, 'js/dashboard_main.js'),
     output: {
         path: path.resolve(__dirname, 'public'),
-        filename: '[name].js',
+        filename: 'bundle.js',
     },
 }

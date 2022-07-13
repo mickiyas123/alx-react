aad3aa7741c2778eeebfed771280f09daa02ddc5
mickiy123 <mikikinfe345@gmail.com>
2022-07-13 16:37:58 +0300

    Update

--- 0x01-react_intro/task_3/dashboard/src/utils.js
+++ 0x01-react_intro/task_3/dashboard/src/utils.js
@@ -1,11 +1,5 @@
-export default function getFooterCopy(isIndex) {
-    return isIndex ? "Holberton School" : "Holberton School main dashboard"
-}
+export const getFooterCopy = (isIndex) => isIndex ? "Holberton School" : "Holberton School main dashboard"
 
-export function getFullYear() {
-    return new Date().getFullYear();
-  }
+export const getFullYear = () => new Date().getFullYear()
 
-export function getLatestNotification() {
-    return "<strong>Urgent requirement</strong> - complete by EOD"
-}
+export const getLatestNotification = () => "<strong>Urgent requirement</strong> - complete by EOD"

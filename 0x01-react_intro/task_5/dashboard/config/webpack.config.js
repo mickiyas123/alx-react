120e671896e19ef0a05fbb687afd5ad16eb06a5d
mickiy123 <mikikinfe345@gmail.com>
2022-07-16 10:05:53 +0300

    Update

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode:  "production",
+    mode:  "development",
     entry: path.join(__dirname, '../src/index.js'),
     output: {
         path: path.join(__dirname, '../dist'),

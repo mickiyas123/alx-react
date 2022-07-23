9ea78c34dac6483209a3a6d873dc255158d3dde3
mickiy123 <mikikinfe345@gmail.com>
2022-07-23 14:05:11 +0300

    Update tests

--- 0x01-react_intro/task_3/dashboard/src/Notifications.test.js
+++ 0x01-react_intro/task_3/dashboard/src/Notifications.test.js
@@ -8,17 +8,18 @@ describe("App", () => {
 
     test("Notifications find ul", () => {
         const wrapper = shallow(<Notifications />);
-        expect(wrapper.find('.ul')).toBeDefined();
+        expect(wrapper.find('ul').length).toBe(1);
     });
 
     test("Notifications renders three list items", () => {
         const wrapper = shallow(<Notifications />);
-        expect(wrapper.find('li')).toHaveLength(3);
+        const unList = wrapper.find('ul')
+        expect(unList.find('li')).toHaveLength(3);
     });
 
     test("Notifications renders text", () => {
         const wrapper = shallow(<Notifications />);
-        const text = wrapper.find('p')
-        expect(text.text()).toBe('Here is the list of notifications');
+        const notificationText = wrapper.find('p')
+        expect(notificationText.text()).toBe('Here is the list of notifications');
     });
 })

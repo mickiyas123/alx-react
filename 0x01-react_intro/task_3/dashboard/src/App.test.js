bd25c46b62b94c8fbf21831d509715fce2a92ef3
mickiy123 <mikikinfe345@gmail.com>
2022-07-23 13:32:34 +0300

    Update tests

--- 0x01-react_intro/task_3/dashboard/src/App.test.js
+++ 0x01-react_intro/task_3/dashboard/src/App.test.js
@@ -8,17 +8,17 @@ describe('App', () => {
 
     test("App renders a div with the class: App-header", () => {
         const wrapper = shallow(<App />);
-        expect(wrapper.find('.App-header')).toBeDefined();
+        expect(wrapper.find('.App-header').length).toBe(1)
     });
 
     test("App renders a div with the class: App-body", () => {
         const wrapper = shallow(<App />);
-        expect(wrapper.find('.App-body')).toBeDefined();
+        expect(wrapper.find('.App-body').length).toBe(1);
     });
 
     test("App renders a div with the class: App-footer", () => {
         const wrapper = shallow(<App />);
-        expect(wrapper.find('.App-footer')).toBeDefined();
+        expect(wrapper.find('.App-footer').length).toBe(1);
     });
  
 });

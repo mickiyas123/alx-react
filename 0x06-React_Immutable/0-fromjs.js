33e2a2cf40fb3b729ef95048089e37b385365751
mickiy123 <mikikinfe345@gmail.com>
2022-08-03 13:53:44 +0300

    Update

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,5 +1,5 @@
 import { fromJS } from 'immutable';
 
-export default function getImmutableObject(object) {
-  return fromJS(object);
+export default function getImmutableObject(newobject) {
+  return fromJS(newobject);
 }

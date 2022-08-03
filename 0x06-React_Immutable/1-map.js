19103bb92367b3a1fdc9b81e9c4185d0d1d64cb6
mickiy123 <mikikinfe345@gmail.com>
2022-08-03 14:45:30 +0300

    Fix: use correct eslint format

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,4 +1,5 @@
-import { Map } from "immutable";
+import { Map } from 'immutable';
+
 export default function getImmutableObject(objectArg) {
-    return Map(objectArg);
+  return Map(objectArg);
 }

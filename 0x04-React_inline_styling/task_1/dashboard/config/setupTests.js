a157af58a1cd2200d14f7a54182a3049fbe9fb71
mickiy123 <mikikinfe345@gmail.com>
2022-08-04 13:29:55 +0300

    Atempt task 1

---
+++ 0x04-React_inline_styling/task_1/dashboard/config/setupTests.js
@@ -0,0 +1,7 @@
+import {StyleSheetTestUtils} from 'aphrodite';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+StyleSheetTestUtils.suppressStyleInjection();
+Enzyme.configure({ adapter: new Adapter() });
+
---
+++ 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import logo from '../assets/holberton-logo.jpg';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  "AppHeader": {
+    display: "flex",
+    alignItems: "center",
+    borderBottom: "4px solid red"
+  },
+  
+  "AppHeaderH1": {
+    color: "red"
+  },
+  Applogo :{
+    width:400,
+    height: 400
+  }
+})
+
+const Header = () => {
+  return (
+    <div className={css(styles.AppHeader)}>
+          <img src={logo} className={css(styles.Applogo)} alt="logo" />
+          <h1 className={css(styles.AppHeaderH1)}>School dashboard</h1>
+    </div>
+  )
+}
+
+export default Header

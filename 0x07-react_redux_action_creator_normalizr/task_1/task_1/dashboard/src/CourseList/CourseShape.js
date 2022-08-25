3fe999edfc0f099bcf67764914032dd92ab48355
mickiy123 <mikikinfe345@gmail.com>
2022-08-25 05:56:23 +0300

    Update

--- 0x07-react_redux_action_creator_normalizr/task_1/task_1/dashboard/src/CourseList/CourseShape.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types'
-
-const CourseShape = PropTypes.shape({
-  id: PropTypes.number.isRequired,
-  length: PropTypes.string.isRequired,
-  credit: PropTypes.number.isRequired
-})
-
-export default CourseShape
